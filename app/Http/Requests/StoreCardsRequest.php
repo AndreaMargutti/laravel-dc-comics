aad94dd8cefaf04500d0a18e8a9e79b347a2e150
AndreaMargutti <marguttiandrea30@gmail.com>
2024-10-31 17:38:45 +0100

    add custom error message + old in form

--- app/Http/Requests/StoreCardsRequest.php
+++ app/Http/Requests/StoreCardsRequest.php
@@ -28,4 +28,21 @@ class StoreCardsRequest extends FormRequest
             "description" => "required|min:4|max:255"
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            "name.required" => "You have to insert a Card Name",
+            "name.min" => "The Card Name has to be at least 3 char long",
+            "name.max" => "The Card Name cannot have more than 255 char",
+            "expansion.min" => "The Expansion has to be at least 3 char long",
+            "expansion.max" => "The Expansion cannot have more than 255 char",
+            "card_type.require" => "You MUST insert a card type",
+            "card_type.min" => "The type has to be at least 3 char long",
+            "card_type.max" => "The type cannot be long more than 255 char",
+            "description.required" => "The description in required",
+            "description.min" => "At least 3 char",
+            "description.max" => "Over the limit of max char"
+        ];
+    }
 }
--- resources/views/cards/create.blade.php
+++ resources/views/cards/create.blade.php
@@ -14,16 +14,20 @@
     <form action="{{route("cards.store")}}" method="POST">
         @csrf
         <label for="card-name" class="form-label">Card Name</label>
-        <input type="text" id="card-name" name="name" class="form-control">
+        <input type="text" id="card-name" name="name" class="form-control"
+        value="{{old('name')}}">
 
         <label for="card-expansion" class="form-label">Card Expansion</label>
-        <input type="text" id="card-expansion" name="expansion" class="form-control">
+        <input type="text" id="card-expansion" name="expansion" class="form-control"
+        value="{{old('expansion')}}">
 
         <label for="card-type" class="form-label">Card Type</label>
-        <input type="text" id="card-type" name="card_type" class="form-control">
+        <input type="text" id="card-type" name="card_type" class="form-control"
+        value="{{old('card_type')}}">
 
         <label for="card-description" class="form-label">Card Description</label>
-        <input type="textarea" id="card-description" name="description" class="form-control">
+        <input type="textarea" id="card-description" name="description" class="form-control"
+        value="{{old('description')}}">
 
         <div class="py-3">
             <button type="submit" class="btn btn-success">Inserisci Carta</button>
